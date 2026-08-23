e1dde5bae9535ae5d48bc517260b8eb0c775fd95
Prity1Kumari <pritykumari2005.yd@gmail.com>
2026-08-24 01:17:25 +0530

    Fix CORS configuration

--- backend/app/main.py
+++ backend/app/main.py
@@ -3,7 +3,11 @@ from fastapi.middleware.cors import CORSMiddleware
 
 from app.routes.predict import router
 from app.api.chat import router as chat_router
-from app.core.config import API_TITLE, API_VERSION
+from app.core.config import API_VERSION
+
+# ==========================
+# FastAPI App
+# ==========================
 
 app = FastAPI(
     title="AI Nutritionist API",
@@ -11,16 +15,15 @@ app = FastAPI(
     description="AI-powered Nutrition Analysis API using Machine Learning and Gemini AI."
 )
 
-app.include_router(chat_router)
-
 # ==========================
 # CORS Configuration
 # ==========================
 
 origins = [
-    "http://localhost:3000",   # React
+    "https://ai-nutritionist-frontend.onrender.com",
+    "http://localhost:3000",
     "http://127.0.0.1:3000",
-    "http://localhost:5173",   # Vite
+    "http://localhost:5173",
     "http://127.0.0.1:5173",
 ]
 
@@ -37,6 +40,7 @@ app.add_middleware(
 # ==========================
 
 app.include_router(router)
+app.include_router(chat_router)
 
 # ==========================
 # Root Endpoint
